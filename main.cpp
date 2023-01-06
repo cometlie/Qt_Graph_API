0e80f3b4a91cc6ea663adf7dcc5efa7362b1f70e
cometlie <1421448986@qq.com>
2023-01-06 20:22:04 +0800

    hashmap second test

--- main.cpp
+++ main.cpp
@@ -50,7 +50,7 @@ int main(int argc, char *argv[])
     ptest.first = "北京";
     ptest.second = 5;
     ptest2.first = "北方";
-    ptest2.second = 0;
+    ptest2.second = 2;
     m.Insert(ptest);
     m.Insert(ptest2);
     std::string test = "北京";
@@ -59,7 +59,7 @@ int main(int argc, char *argv[])
     qDebug()<<m.getValue(test);
     qDebug()<<m.getValue(test2);
     m.Remove(test2);
-//    qDebug()<<m.getValue(test2);
+    qDebug()<<m.getValue(test2);
     return a.exec();
 }
 
